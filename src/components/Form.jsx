f840e6b0f5039a7b502f3c63488c3d725183d305
Ali Tahir <alitahir6001@gmail.com>
2021-03-15 12:58:44 -0400

    First run through of the forms + hooks assignment

---
+++ src/components/Form.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Form = (props) => {
+
+    const {inputs, setInputs} = props;
+
+    const onChange = e => {
+        setInputs({
+            ...inputs, // Bring it all in  (spread)
+            [e.target.name]: e.target.value // Targeting the specific thing I want
+        });
+    };
+    return(
+        <form>
+            <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input onChange={onChange} type="text" name="firstName" />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input onChange={onChange} type="text" name="lastName" />
+            </div>
+            
+
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input onChange={onChange} type="text" name="email" />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input onChange={onChange} type="password" name="password" />
+            </div>
+            
+            <div className="form-group">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input onChange={onChange} type="password" name="confirmPassword" />
+            </div>
+        </form>
+    );
+};
+
+
+export default Form;
